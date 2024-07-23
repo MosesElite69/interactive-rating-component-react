4f971cc848e896dc47ea4ff9d0903c6c57db2649
elliotedwinmoses2020 <elliotedwinmoses2020@gmail.com>
2024-07-23 10:37:53 +0000

    changed some stuff

---
+++ src/ui/Thanks.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Thanks = () => {
+  return (
+    <>
+    // Thank you state start
+
+You selected // Add rating here 
+
+ out of 5
+
+Thank you!
+
+We appreciate you taking the time to give a rating. If you ever need more support, 
+don’t hesitate to get in touch!
+
+// Thank you state end 
+
+    </>
+  )
+}
+
+export default Thanks
